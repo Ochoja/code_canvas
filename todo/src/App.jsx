2319c83a2284502044086a2eddd786ffd46546a1
Ochoja <danielochoja@yahoo.com>
2025-01-30 04:36:28 +0100

    added todoInput logic

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header"
-import {TodoList} from './components/TodoList'
-import {Tabs} from './components/Tabs'
-import {TodoInput} from './components/TodoInput'
+import { TodoList } from './components/TodoList'
+import { Tabs } from './components/Tabs'
+import { TodoInput } from './components/TodoInput'
+import { useState } from 'react'
 
 export default function App() {
-  const todos = [
-                  {input: 'Clean up the kitchen', complete: true},
-                  {input: 'Wash some plates', complete: false},
-                  {input: 'Do the dishes', complete: true}
-                ]
+  // const todos = [
+  //                 {input: 'Clean up the kitchen', complete: true},
+  //                 {input: 'Wash some plates', complete: false},
+  //                 {input: 'Do the dishes', complete: true}
+  //               ]
+
+  const [todos, setTodos] = useState([{input: 'Clean up the kitchen', complete: true}])
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, {input: newTodo, complete: false}]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos}  />
-      <TodoInput />
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   )
 }
--- todo/src/components/TodoInput.jsx
+++ todo/src/components/TodoInput.jsx
@@ -1,8 +1,19 @@
-export function TodoInput() {
+import { useState } from "react"
+
+export function TodoInput(props) {
+  const { handleAddTodo } = props
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <div className="input-container">
-      <input type="text" placeholder="Add Task" />
-      <button>Add Todo</button>
+      <input type="text" value={inputValue} onChange={(e) => {
+        setInputValue(e.target.value)
+      }} placeholder="Add Task"  />
+
+      <button onClick={() => {
+        handleAddTodo(inputValue)
+        setInputValue('')
+      }}>Add Todo</button>
     </div>
   )
 }
