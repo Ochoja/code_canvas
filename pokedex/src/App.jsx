56d1be35856df72d25a7d977f49968b7f338a1c6
Ochoja <danielochoja@yahoo.com>
2025-03-03 23:16:39 +0100

    updated App.jsx

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -2,11 +2,18 @@ import { Header } from './components/Header';
 import { SideNav } from './components/SideNav';
 import { PokeCard } from './components/PokeCard';
 
+import { useState } from 'react';
+
 function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
+
   return (
     <>
       <Header />
-      <SideNav />
+      <SideNav
+        selectedPokemon={selectedPokemon}
+        setSelectedPokemon={setSelectedPokemon}
+      />
       <PokeCard />
     </>
   );
