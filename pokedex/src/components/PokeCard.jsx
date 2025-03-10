2e903fe57df719147509ab8b9665bbcdfa9963a1
Ochoja <danielochoja@yahoo.com>
2025-03-10 05:22:48 +0100

    updated

--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -1,4 +1,42 @@
+import { useEffect, useState } from 'react';
+
 export function PokeCard(props) {
   const { selectedPokemon } = props;
-  return <div></div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (loading || !localStorage) return;
+
+    let cache = {};
+    if (localStorage.getItem('pokedex')) {
+      cache = JSON.parse(localStorage.getItem('pokedex'));
+    }
+
+    if (selectedPokemon in cache) {
+      setData(cache[selectedPokemon]);
+      return;
+    }
+
+    async function fetchPokemonData() {
+      setLoading(true);
+      try {
+        const baseUrl = 'https://pokeapi.co/api/v2/';
+        const suffix = `pokemon/${selectedPokemon}`;
+        const finalUrl = baseUrl + suffix;
+        const res = await fetch(finalUrl);
+        const pokemonData = await res.json();
+        setData(pokemonData);
+        cache[selectedPokemon] = pokemonData;
+        localStorage.setItem('pokedex', JSON.stringify(cache));
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPokemonData();
+  }, [loading, selectedPokemon]);
+  return <div>{data}</div>;
 }
