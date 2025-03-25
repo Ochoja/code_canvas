c438050706d4cdedd76a1919fa7b98314822ca8a
Ochoja <danielochoja@yahoo.com>
2025-03-25 18:44:34 +0100

    fixed bug

--- pokedex/src/components/Modal.jsx
+++ pokedex/src/components/Modal.jsx
@@ -1,7 +1,13 @@
-export function Modal() {
-  return (
-    <div>
-      <h2>Modal</h2>
-    </div>
+import ReactDom from 'react-dom';
+
+export function Modal(props) {
+  const { children, handleCloseModal } = props;
+
+  return ReactDom.createPortal(
+    <div className='modal-container'>
+      <button onClick={handleCloseModal} className='modal-underlay'></button>
+      <div className='modal-content'>{children}</div>
+    </div>,
+    document.getElementById('portal')
   );
 }
--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -1,27 +1,16 @@
 import { useEffect, useState } from 'react';
 import { getFullPokedexNumber } from '../utils';
 import { TypeCard } from './TypeCard';
+import { Modal } from './Modal';
 
-export function PokeCard(props) {
-  const { selectedPokemon } = props;
+export function PokeCard({ selectedPokemon }) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const { name, sprites, stats, types, moves, height, abilities } = data || {};
-
-  const imgList = Object.keys(sprites || {}).filter((val) => {
-    if (!sprites[val]) return false;
-    if (['versions', 'other'].includes(val)) return false;
-    return true;
-  });
 
   useEffect(() => {
     if (!localStorage) return;
 
-    let cache = {};
-    if (localStorage.getItem('pokedex')) {
-      cache = JSON.parse(localStorage.getItem('pokedex'));
-    }
+    let cache = JSON.parse(localStorage.getItem('pokedex') || '{}');
 
     if (selectedPokemon in cache) {
       setData(cache[selectedPokemon]);
@@ -31,18 +20,15 @@ export function PokeCard(props) {
     async function fetchPokemonData() {
       setLoading(true);
       try {
-        const baseUrl = 'https://pokeapi.co/api/v2/';
-        const suffix = `pokemon/${selectedPokemon + 1}`;
-        const finalUrl = baseUrl + suffix;
-        const res = await fetch(finalUrl);
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`
+        );
         const pokemonData = await res.json();
-
         setData(pokemonData);
-        console.log(pokemonData);
         cache[selectedPokemon] = pokemonData;
         localStorage.setItem('pokedex', JSON.stringify(cache));
       } catch (error) {
-        console.log(error.message);
+        console.error('Error fetching Pokemon data:', error);
       } finally {
         setLoading(false);
       }
@@ -51,62 +37,73 @@ export function PokeCard(props) {
     fetchPokemonData();
   }, [selectedPokemon]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className='poke-card'>
+  if (loading) return <div>Loading...</div>;
+  if (!data) return <div>No data found</div>;
+
+  const { name, sprites, stats, types, moves } = data;
+  const imgList = Object.keys(sprites || {}).filter(
+    (val) => sprites[val] && !['versions', 'other'].includes(val)
+  );
+
+  return (
+    <div className='poke-card'>
+      <Modal handleCloseModal={() => {}}>
         <div>
-          <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
+          <h6>Name</h6>
           <h2>{name}</h2>
         </div>
-        <div className='type-container'>
-          {data?.types.map((type, typeIndex) => {
-            return <TypeCard key={typeIndex} type={type.type.name} />;
-          })}
+        <div>
+          <h6>Description</h6>
+          <p>Actual</p>
         </div>
-        <img
-          className='default-img'
-          src={'/pokemon/' + getFullPokedexNumber(selectedPokemon) + '.png'}
-          alt=''
-        />
-        <div className='img-container'>
-          {imgList.map((spriteUrl, spriteIndex) => {
-            return (
-              <img
-                key={spriteIndex}
-                src={sprites[spriteUrl]}
-                alt='Some sprite'
-              />
-            );
-          })}
-        </div>
-        <h3>Stats</h3>
-        <div className='stats-card'>
-          {stats?.map((statObj, statIndex) => {
-            const { stat, base_stat } = statObj;
-            return (
-              <div className='stat-item' key={statIndex}>
-                <p>{stat?.name.replaceAll('-', ' ')}</p>
-                <h4>{base_stat}</h4>
-              </div>
-            );
-          })}
-        </div>
-        <h3>Moves</h3>
-        <div className='pokemon-move-grid'>
-          {moves.map((moveObj, moveIndex) => {
-            return (
-              <button
-                key={moveIndex}
-                className='button-card pokemon-move'
-                onClick={() => {}}>
-                <p>{moveObj.move?.name.replaceAll('-', ' ')}</p>
-              </button>
-            );
-          })}
-        </div>
+      </Modal>
+
+      <div>
+        <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
+        <h2>{name}</h2>
       </div>
-    );
-  }
+
+      <div className='type-container'>
+        {types?.map((type, index) => (
+          <TypeCard key={index} type={type.type.name} />
+        ))}
+      </div>
+
+      <img
+        className='default-img'
+        src={sprites?.front_default || '/placeholder.png'}
+        alt={name}
+      />
+
+      <div className='img-container'>
+        {imgList.map((spriteUrl, index) =>
+          sprites && sprites[spriteUrl] ? (
+            <img key={index} src={sprites[spriteUrl]} alt='Sprite' />
+          ) : null
+        )}
+      </div>
+
+      <h3>Stats</h3>
+      <div className='stats-card'>
+        {stats?.map((statObj, index) => (
+          <div className='stat-item' key={index}>
+            <p>{statObj.stat.name.replaceAll('-', ' ')}</p>
+            <h4>{statObj.base_stat}</h4>
+          </div>
+        ))}
+      </div>
+
+      <h3>Moves</h3>
+      <div className='pokemon-move-grid'>
+        {moves?.slice(0, 10).map((moveObj, index) => (
+          <button
+            key={index}
+            className='button-card pokemon-move'
+            onClick={() => {}}>
+            <p>{moveObj.move.name.replaceAll('-', ' ')}</p>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
