8de8710b7595e5b8548406ed671b4df606c03843
Ochoja <danielochoja@yahoo.com>
2025-04-22 15:57:57 +0100

    init repo

---
+++ product-page/src/App.jsx
@@ -0,0 +1,11 @@
+import { Navigation } from './components/Navigation';
+
+function App() {
+  return (
+    <div className='mx-[10%]'>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ product-page/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+export function Navigation() {
+  return (
+    <nav className='flex justify-between'>
+      <div>
+        <div></div>
+        <div className='flex gap-'>
+          <div>Collection</div>
+          <div>Men</div>
+          <div>Women</div>
+          <div>About</div>
+          <div>Contact</div>
+        </div>
+      </div>
+      <div></div>
+    </nav>
+  );
+}
