f4bf253b5158f21cade76ddf5f5380e9b16566f1
Ochoja <danielochoja@yahoo.com>
2025-04-24 14:00:09 +0100

    added product page

--- product-page/src/components/Navigation.jsx
+++ product-page/src/components/Navigation.jsx
@@ -1,17 +1,34 @@
+import logo from '../assets/logo.svg';
+import cartLogo from '../assets/icon-cart.svg';
+import avatar from '../assets/image-avatar.png';
+
 export function Navigation() {
+  const navItems = ['Collections', 'Men', 'Women', 'About', 'Contact'];
+
   return (
-    <nav className='flex justify-between'>
-      <div>
-        <div></div>
-        <div className='flex gap-'>
-          <div>Collection</div>
-          <div>Men</div>
-          <div>Women</div>
-          <div>About</div>
-          <div>Contact</div>
+    <nav className='flex justify-between items-center border-b-1'>
+      <div className='flex gap-12 items-center'>
+        <div>
+          <img src={logo} alt='Logo' />
+        </div>
+        <div className='flex justify-center gap-4'>
+          {navItems.map((item, itemIndex) => {
+            return (
+              <div key={itemIndex} className='py-10'>
+                {item}
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div></div>
+      <div className='flex gap-8 items-center'>
+        <div>
+          <img src={cartLogo} alt='CartLogo' />
+        </div>
+        <div className='max-h-12 max-w-12'>
+          <img src={avatar} alt='CartLogo' />
+        </div>
+      </div>
     </nav>
   );
 }
